f1514b9bd9586136036b52d40547442539c503e5
ian akoto <iancecilakoto@gmail.com>
2020-05-21 10:38:01 +0000

    serverless database attached and get and create todos lambda functions added

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -4,9 +4,34 @@ import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } f
 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 
+import * as AWS from 'aws-sdk'
+
+
+const doClient= new AWS.DynamoDB.DocumentClient()
+
+const todosTable = process.env.TODOS_TABLE
+
+
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
 
   // TODO: Implement creating a new TODO item
-  return undefined
+
+
+  await doClient.put({
+    TableName: todosTable,
+    Item: newTodo
+  }).promise()
+
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      newTodo
+    })
+  }
+
 }
--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,79 @@
 import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS from 'aws-sdk'
+
+
+const doClient= new AWS.DynamoDB.DocumentClient()
+
+const todosTable = process.env.TODOS_TABLE
+
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
+  
+  const todoId = event.pathParameters.todoId
+
+  const valiGroupId = await todoExisit(todoId)
+
+
+  if(!valiGroupId) {
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        error: 'Todo does not exist'
+      })
+    }
+  }
+
+
+  const todoItems = await getItemsPerTodo(todoId)
+  
+ 
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      items: todoItems
+    })
+  }
+
+
+
+}
+
+
+async function todoExisit(todoId: string) {
+
+  const result = await doClient
+  .get({
+    TableName: todosTable,
+    Key: {
+      id: todoId
+    }
+  }).promise()
+
+  console.log('Get group:', result)
+  return !!result.Item
+
+
+}
+
+
+async function getItemsPerTodo(todoId: string) {
+  const result = await doClient.query({
+    TableName: todosTable,
+    KeyConditionExpression: 'todoId = :todoId',
+    ExpressionAttributeValues: {
+      ':todoId': todoId
+    },
+    ScanIndexForward: false
+  }).promise()
+
+  return result.Items
 }
