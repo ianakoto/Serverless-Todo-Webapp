e82a67119c058f6792fe9062572bad54e772fc2e
ian akoto <iancecilakoto@gmail.com>
2020-05-28 20:03:38 +0000

    extract userId from response head

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -2,7 +2,9 @@ import 'source-map-support/register'
 import { APIGatewayProxyResult, APIGatewayProxyHandler, APIGatewayEvent } from 'aws-lambda'
 import * as AWS from 'aws-sdk'
 import * as AWSXRay from 'aws-xray-sdk'
+import { decode } from 'jsonwebtoken'
 
+import { JwtPayload } from './../../auth/JwtPayload'
 import { createLogger } from '../../utils/logger'
 const logger = createLogger('getTodo')
 
@@ -19,7 +21,10 @@ const todoTableIndex = process.env.TODO_ID_INDEX
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
   logger.info(`todo get body: ${JSON.parse(event.body)}`)
-  const userId = event.pathParameters.userId
+
+  const authHeader = event.headers.Authorization
+  const authSplit = authHeader.split(" ")
+  const userId = parseUserId(authSplit[1])
 
 
 
@@ -67,4 +72,18 @@ async function getItemsPerTodo(userId: String) {
 
   logger.info(`todo results: ${result.items}`)
   return result
+}
+
+
+
+
+
+/**
+ * Parse a JWT token and return a user id
+ * @param jwtToken JWT token to parse
+ * @returns a user id from the JWT token
+ */
+export function parseUserId(jwtToken: string): string {
+  const decodedJwt = decode(jwtToken) as JwtPayload
+  return decodedJwt.sub
 }
